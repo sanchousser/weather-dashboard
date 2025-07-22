3f619695347b972e5d11bf9e60d1cf64c063c90b
sanchousser <golyukaleks@gmail.com>
2025-07-22 18:32:50 +0200

    upd

--- src/components/Weather/WeatherCard/WeatherCard.jsx
+++ src/components/Weather/WeatherCard/WeatherCard.jsx
@@ -1,6 +1,7 @@
-import { IoRefresh } from "react-icons/io5";
+import { MdRefresh } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { IoSunnySharp } from "react-icons/io5";
 
 import css from './WeatherCard.module.css'
 
@@ -16,21 +17,23 @@ export const WeatherCard = () => {
                 <button>Hourly forecast</button>
                 <button>Weekly forecast</button>
             </div>
-            <div >
+            <div className={css.date}>
                 <p>Date</p>
-                <p>|</p>
+                <span>|</span>
                 <p>Day</p>
             </div>
 
-            <img src="" alt="" />
+            <div className={css.temperature__icon__div}>
+                <IoSunnySharp className={css.temperature__icon} />
+            </div>
 
-            <p>temperature</p>
+            <p className={css.temperature}>temperature</p>
 
-            <div>
-                <IoRefresh />
-                <FaRegHeart />
+            <div className={css.weather__btns}>
+                <MdRefresh className={css.refresh}/>
+                <FaRegHeart className={css.heart}/>
                 <button>See more</button>
-                <RiDeleteBin6Line />
+                <RiDeleteBin6Line className={css.bin}/>
 
             </div>
 
