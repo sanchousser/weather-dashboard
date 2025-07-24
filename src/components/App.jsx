dad8c7faa79aa9dec1c15d7db6b088416c9090c4
anastasia <anastasiaverbenets@gmail.com>
2025-07-24 22:05:03 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-import News from 'News/News';
+import { WeatherList } from './Weather/WeatherList/WeatherList';
 import Container from './Container/Container';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Hero from './Hero/Hero';
+import { useState } from 'react';
+
+import { fetchCoordinates, fetchWeather } from '../services/getWeatherContent';
 
 export const App = () => {
+  const [query, setQuery] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [cityInfo, setCityInfo] = useState(null);
+
+  const handleChange = e => setQuery(e.target.value);
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const coords = await fetchCoordinates(query);
+      const weather = await fetchWeather(coords);
+      setCityInfo(coords);
+      setWeatherData(weather);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <>
       <Container>
         <Header />
-        <Hero />
-        <News />
+        <Hero query={query} onChange={handleChange} onSubmit={handleSubmit} />
+        {weatherData && cityInfo && (
+          <WeatherList weatherData={weatherData} cityInfo={cityInfo} />
+        )}
         <Footer />
       </Container>
     </>
