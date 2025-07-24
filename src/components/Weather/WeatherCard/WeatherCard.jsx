3b77db4a122adba525fedf5bbee79802fed2cdc6
sanchousser <golyukaleks@gmail.com>
2025-07-24 18:10:11 +0200

    upd

--- src/components/Weather/WeatherCard/WeatherCard.jsx
+++ src/components/Weather/WeatherCard/WeatherCard.jsx
@@ -1,39 +1,51 @@
 import { MdRefresh } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { IoSunnySharp } from "react-icons/io5";
+// import { IoSunnySharp } from "react-icons/io5";
+
+import { format } from 'date-fns';
 
 import css from './WeatherCard.module.css'
 
-export const WeatherCard = () => {
+export const WeatherCard = ({ data, city }) => {
+
+    if (!data?.current || !city) {
+        return <li>Loading weather data...</li>;
+    }
+
+    const dt = new Date(data.current.dt * 1000);
+    const icon = data.current.weather?.[0]?.icon;
+    const description = data.current.weather?.[0]?.description;
+
     return (
         <li className={css.weather__card}>
             <div className={css.place__text}>
-                <p>City</p>
-                <p>Country</p>
+                <p>{city.name}</p>
+                <p>{city.country}</p>
             </div>
-            <p className={css.time}>Time</p>
+            <p className={css.time}>{format(dt, 'HH:mm')}</p>
             <div className={css.forecast__btns}>
                 <button>Hourly forecast</button>
                 <button>Weekly forecast</button>
             </div>
             <div className={css.date}>
-                <p>Date</p>
+                <p>{format(dt, 'dd.MM.yyyy')}</p>
                 <span>|</span>
-                <p>Day</p>
+                <p>{format(dt, 'EEEE')}</p>
             </div>
 
             <div className={css.temperature__icon__div}>
-                <IoSunnySharp className={css.temperature__icon} />
+                {/* <IoSunnySharp className={css.temperature__icon} /> */}
+                <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={description} />
             </div>
 
-            <p className={css.temperature}>temperature</p>
+            <p className={css.temperature}>{Math.round(data.current.temp)}℃</p>
 
             <div className={css.weather__btns}>
-                <MdRefresh className={css.refresh}/>
-                <FaRegHeart className={css.heart}/>
+                <MdRefresh className={css.refresh} />
+                <FaRegHeart className={css.heart} />
                 <button>See more</button>
-                <RiDeleteBin6Line className={css.bin}/>
+                <RiDeleteBin6Line className={css.bin} />
 
             </div>
 
