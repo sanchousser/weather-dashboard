f767219a2cb08753b6164157f5295bd81465f1a2
sanchousser <golyukaleks@gmail.com>
2025-07-29 17:58:48 +0200

    upd

--- src/components/Weather/WeatherStats/WeatherStats.jsx
+++ src/components/Weather/WeatherStats/WeatherStats.jsx
@@ -1,4 +1,5 @@
 import css from './WeatherStats.module.css'
+import warmIcon from '../../../imgs/weather/warm.png'
 
 
 export const WeatherStats = () => {
@@ -8,7 +9,7 @@ export const WeatherStats = () => {
                 <li className={css.stats__item}>
                     <p className={css.item__title}>Feels like</p>
                     <p className={css.item__comment}>29,2</p>
-                    <img className={css.item__icon} src={`/imgs/weather/cold.png`} alt="" />
+                    <img className={css.item__icon} src={warmIcon} alt="" />
                 </li>
             </ul>
         </div>
