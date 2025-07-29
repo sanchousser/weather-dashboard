b925f179d55748bc8c2e676e6ca288be67f8f966
sanchousser <golyukaleks@gmail.com>
2025-07-29 12:11:33 +0200

    upd

--- src/components/Weather/WeatherCard/WeatherCard.jsx
+++ src/components/Weather/WeatherCard/WeatherCard.jsx
@@ -1,51 +1,63 @@
 import { MdRefresh } from "react-icons/md";
-import { FaRegHeart } from "react-icons/fa";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
-// import { IoSunnySharp } from "react-icons/io5";
+
 
 import { format } from 'date-fns';
 
 import css from './WeatherCard.module.css'
+import { useState } from "react";
 
-export const WeatherCard = ({ data, city }) => {
+// export const WeatherCard = ({ data, city }) => {
+export const WeatherCard = ({onCardDelete, weatherCard}) => {
 
-    if (!data?.current || !city) {
+    const [liked, setLiked] = useState(false);
+
+
+    if (!weatherCard.data?.current || !weatherCard.name) {
         return <li>Loading weather data...</li>;
     }
 
-    const dt = new Date(data.current.dt * 1000);
-    const icon = data.current.weather?.[0]?.icon;
-    const description = data.current.weather?.[0]?.description;
+    const onHeartClick = (e) => {
+        e.preventDefault()
+        setLiked(prev => !prev);
+    }
+
+    // const dt = new Date(weatherCarddata.current.dt * 1000);
+    // const icon = data.current.weather?.[0]?.icon;
+    // const description = data.current.weather?.[0]?.description;
+    // console.log('dt:', dt)
 
     return (
         <li className={css.weather__card}>
             <div className={css.place__text}>
-                <p>{city.name}</p>
-                <p>{city.country}</p>
+                <p>{weatherCard.name}</p>
+                <p>{weatherCard.country}</p>
             </div>
-            <p className={css.time}>{format(dt, 'HH:mm')}</p>
+            <p className={css.time}>{format(weatherCard.data.dt, 'HH:mm')}</p>
             <div className={css.forecast__btns}>
                 <button>Hourly forecast</button>
                 <button>Weekly forecast</button>
             </div>
             <div className={css.date}>
-                <p>{format(dt, 'dd.MM.yyyy')}</p>
+                <p>{format(weatherCard.data.dt, 'dd.MM.yyyy')}</p>
                 <span>|</span>
-                <p>{format(dt, 'EEEE')}</p>
+                <p>{format(weatherCard.data.dt, 'EEEE')}</p>
             </div>
 
             <div className={css.temperature__icon__div}>
                 {/* <IoSunnySharp className={css.temperature__icon} /> */}
-                <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} className={css.temperature__icon} alt={description} />
+                <img src={`http://openweathermap.org/img/wn/${weatherCard.icon}@2x.png`} className={css.temperature__icon} alt={weatherCard.description} />
             </div>
 
-            <p className={css.temperature}>{Math.round(data.current.temp)}℃</p>
+            <p className={css.temperature}>{Math.round(weatherCard.data.current.temp)}℃</p>
 
             <div className={css.weather__btns}>
                 <MdRefresh className={css.refresh} />
-                <FaRegHeart className={css.heart} />
+                {/* <FaRegHeart onClick={onHeartClick} className={css.heart} /> */}
+                {liked ? <FaHeart onClick={onHeartClick} color="red" className={css.liked}/> : <FaRegHeart onClick={onHeartClick} className={css.heart}/>}
                 <button>See more</button>
-                <RiDeleteBin6Line className={css.bin} />
+                <RiDeleteBin6Line onClick={() => onCardDelete(weatherCard.name)} className={css.bin} />
 
             </div>
 
