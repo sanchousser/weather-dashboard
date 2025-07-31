05f162929b495f1c33e43fde93851b22ec7647e9
anastasia <anastasiaverbenets@gmail.com>
2025-07-31 18:33:02 +0300

    upd container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,32 +7,25 @@ import Hero from './Hero/Hero';
 
 import { useEffect, useState } from 'react';
 
-
-
-import { fetchCoordinates, fetchWeather } from '../services/getWeatherContent'
+import { fetchCoordinates, fetchWeather } from '../services/getWeatherContent';
 import { WeatherStats } from './Weather/WeatherStats/WeatherStats';
 
 import News from 'News/News';
-
 
 export const App = () => {
   const [query, setQuery] = useState('');
   // const [weatherData, setWeatherData] = useState(null);
   // const [cityInfo, setCityInfo] = useState(null);
 
-
-   const [weatherCards, setWeatherCards] = useState(() => {
+  const [weatherCards, setWeatherCards] = useState(() => {
     return JSON.parse(window.localStorage.getItem('weatherCards')) ?? [];
   });
-  const [seeMore, setSeeMore] = useState(false)
+  const [seeMore, setSeeMore] = useState(false);
   const [cityToSeeMore, setCityToSeeMore] = useState('');
-
-
 
   useEffect(() => {
     window.localStorage.setItem('weatherCards', JSON.stringify(weatherCards));
   }, [weatherCards]);
-
 
   useEffect(() => {
     window.localStorage.setItem('weatherCards', JSON.stringify(weatherCards));
@@ -55,11 +48,10 @@ export const App = () => {
       const description = weather.current.weather?.[0]?.description;
 
       setWeatherCards(prevCards => {
-
-
-
-
-        if (window.matchMedia("(width <= 768px)").matches && window.matchMedia("(width > 320px)").matches) {
+        if (
+          window.matchMedia('(width <= 768px)').matches &&
+          window.matchMedia('(width > 320px)').matches
+        ) {
           return [
             {
               ...coords,
@@ -101,48 +93,47 @@ export const App = () => {
     }
   };
 
-
-
-  const handleCardDelete = (name) => {
-    setWeatherCards(prevCards =>
-      prevCards.filter(card => card.name !== name)
-    );
+  const handleCardDelete = name => {
+    setWeatherCards(prevCards => prevCards.filter(card => card.name !== name));
 
     setSeeMore(false);
-  }
+  };
 
-  const toggleSeeMore = (cityName) => {
+  const toggleSeeMore = cityName => {
     if (seeMore && cityToSeeMore !== cityName) {
-      setCityToSeeMore(cityName)
+      setCityToSeeMore(cityName);
     } else {
-      setSeeMore(prev => !prev)
-      setCityToSeeMore(cityName)
+      setSeeMore(prev => !prev);
+      setCityToSeeMore(cityName);
     }
-  }
+  };
 
-  const findByName = name => weatherCards.find(card => card.name === name)
-
-
-
+  const findByName = name => weatherCards.find(card => card.name === name);
 
   return (
     <>
       <Container>
         <Header />
-        <Hero query={query} onChange={handleChange} onSubmit={handleSubmit} />
-
+      </Container>
+      <Hero query={query} onChange={handleChange} onSubmit={handleSubmit} />
+      <Container>
         {/* {weatherData && cityInfo && <WeatherList weatherData={weatherData} cityInfo={cityInfo} />} */}
 
-
-        {weatherCards.length !== 0 && <WeatherList onCardDelete={handleCardDelete} weatherCards={weatherCards} toggleSeeMore={toggleSeeMore} />}
-        {seeMore && cityToSeeMore && <WeatherStats weatherCard={findByName(cityToSeeMore)} />}
-
-
+        {weatherCards.length !== 0 && (
+          <WeatherList
+            onCardDelete={handleCardDelete}
+            weatherCards={weatherCards}
+            toggleSeeMore={toggleSeeMore}
+          />
+        )}
+        {seeMore && cityToSeeMore && (
+          <WeatherStats weatherCard={findByName(cityToSeeMore)} />
+        )}
 
         <News />
-
-        <Footer />
       </Container>
+
+      <Footer />
     </>
   );
 };
